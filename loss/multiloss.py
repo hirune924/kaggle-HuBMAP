58b84c4a96face69d53b2297dae4cc0f0d76a327
hirune924 <36130517+hirune924@users.noreply.github.com>
2020-12-29 17:48:36 +0900

    Update multiloss.py

--- loss/multiloss.py
+++ loss/multiloss.py
@@ -1,31 +1,19 @@
 import torch
 from torch import nn
 import numpy as np
+import catalyst.contrib.nn.criterion.dice.BCEDiceLoss as BCEDiceLoss
 
-
-class BDLoss(nn.Module):
+class MultiLoss(nn.Module):
     def __init__(self):
-        """
-        compute boudary loss
-        only compute the loss of foreground
-        ref: https://github.com/LIVIAETS/surface-loss/blob/108bd9892adca476e6cdf424124bc6268707498e/losses.py#L74
-        """
-        super(BDLoss, self).__init__()
-        # self.do_bg = do_bg
+        super(MultiLoss, self).__init__()
+        self.seg_criteria = BCEDiceLoss()
+        self.cls_criteria = nn.BCEWithLogitsLoss()
 
-    def forward(self, net_output, target, bound):
-        """
-        net_output: (batch_size, class, x,y,z)
-        target: ground truth, shape: (batch_size, 1, x,y,z)
-        bound: precomputed distance map, shape (batch_size, class, x,y,z)
-        """
-        net_output = torch.sigmoid(net_output)
-        # print('net_output shape: ', net_output.shape)
-        pc = net_output[:, :, ...].type(torch.float32)
-        dc = bound[:,:, ...].type(torch.float32)
+    def forward(self, y_hat, y, y_label, label):
+        seg_loss = self.seg_criteria(y_hat, y)
+        cls_loss = self.cls_criteria(y_label, label)
+        
+        loss = cls_loss + seg_loss
 
-        multipled = torch.einsum("bcxyz,bcxyz->bcxyz", pc, dc)
-        bd_loss = multipled.mean()
-
-        return bd_loss
+        return loss
 
