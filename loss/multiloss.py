3c25ef5cacd28c9e7fac7b0d1c43ccdacded4150
hirune924 <36130517+hirune924@users.noreply.github.com>
2020-12-29 22:39:43 +0900

    Update multiloss.py

--- loss/multiloss.py
+++ loss/multiloss.py
@@ -17,3 +17,23 @@ class MultiLoss(nn.Module):
 
         return loss
 
+class OUSMMultiLoss(nn.Module):
+    def __init__(self):
+        super(OUSMMultiLoss, self).__init__()
+        self.seg_criteria = BCEDiceLoss()
+        self.cls_criteria = nn.BCEWithLogitsLoss()
+
+    def forward(self, y_hat, y, y_label, label):
+        bs = y_label.shape[0]
+        losses = self.cls_criteria(y_label, label, reduction='none')
+        if len(losses.shape) == 2:
+            losses = losses.mean(1)
+        _, idxs = losses.topk(int(bs*0.8), largest=False)
+        
+        cls_loss = losses.index_select(0, idxs).mean()
+        
+        seg_loss = self.seg_criteria(y_hat.index_select(0, idxs), y.index_select(0, idxs))
+        
+        loss = cls_loss + seg_loss
+
+        return loss
