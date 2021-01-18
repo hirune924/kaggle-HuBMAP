d3f7190d5da841974888f3b10c14473a966cf32d
hirune924 <36130517+hirune924@users.noreply.github.com>
2021-01-18 18:26:15 +0900

    Update train.py

--- train.py
+++ train.py
@@ -17,51 +17,76 @@ from utils.utils import custom_load
 from torchvision.datasets import CIFAR10
 from torchvision import transforms
 from torch.utils.data import DataLoader, random_split
+import torch
 
 class MyIter(object):
-  """An iterator."""
-  def __init__(self, my_loader):
-    self.my_loader = my_loader
-    self.loader_iters = [iter(loader) for loader in self.my_loader.loaders]
+    """An iterator."""
+    def __init__(self, my_loader):
+      self.my_loader = my_loader
+      self.loader_iters = [iter(loader) for loader in self.my_loader.loaders]
 
-  def __iter__(self):
-    return self
+    def __iter__(self):
+      return self
 
-  def __next__(self):
-    # When the shortest loader (the one with minimum number of batches)
-    # terminates, this iterator will terminates.
-    # The `StopIteration` raised inside that shortest loader's `__next__`
-    # method will in turn gets out of this `__next__` method.
-    batches = [loader_iter.next() for loader_iter in self.loader_iters]
-    return self.my_loader.combine_batch(batches)
+    def __next__(self):
+      # When the shortest loader (the one with minimum number of batches)
+      # terminates, this iterator will terminates.
+      # The `StopIteration` raised inside that shortest loader's `__next__`
+      # method will in turn gets out of this `__next__` method.
+      batches = [loader_iter.next() for loader_iter in self.loader_iters]
+      return self.my_loader.combine_batch(batches)
 
-  # Python 2 compatibility
-  next = __next__
+    # Python 2 compatibility
+    next = __next__
 
-  def __len__(self):
-    return len(self.my_loader)
+    def __len__(self):
+      return len(self.my_loader)
 
   
 class MyLoader(object):
-  """This class wraps several pytorch DataLoader objects, allowing each time 
-  taking a batch from each of them and then combining these several batches 
-  into one. This class mimics the `for batch in loader:` interface of 
-  pytorch `DataLoader`.
-  Args: 
-    loaders: a list or tuple of pytorch DataLoader objects
-  """
-  def __init__(self, loaders):
-    self.loaders = loaders
+    """This class wraps several pytorch DataLoader objects, allowing each time 
+    taking a batch from each of them and then combining these several batches 
+    into one. This class mimics the `for batch in loader:` interface of 
+    pytorch `DataLoader`.
+    Args: 
+      loaders: a list or tuple of pytorch DataLoader objects
+    """
+    def __init__(self, loaders):
+      self.loaders = loaders
 
-  def __iter__(self):
-    return MyIter(self)
+    def __iter__(self):
+      return MyIter(self)
 
-  def __len__(self):
-    return min([len(loader) for loader in self.loaders])
+    def __len__(self):
+      return min([len(loader) for loader in self.loaders])
 
-  # Customize the behavior of combining batches here.
-  def combine_batch(self, batches):
-    return batches
+    # Customize the behavior of combining batches here.
+    def combine_batch(self, batches):
+      return batches
+
+class EMAWeightOptimizer (object):
+    def __init__(self, target_net, source_net, ema_alpha):
+        self.target_net = target_net
+        self.source_net = source_net
+        self.ema_alpha = ema_alpha
+        self.target_params = [p for p in target_net.state_dict().values() if p.dtype == torch.float]
+        self.source_params = [p for p in source_net.state_dict().values() if p.dtype == torch.float]
+
+        for tgt_p, src_p in zip(self.target_params, self.source_params):
+            tgt_p[...] = src_p[...]
+
+        target_keys = set(target_net.state_dict().keys())
+        source_keys = set(source_net.state_dict().keys())
+        if target_keys != source_keys:
+            raise ValueError('Source and target networks do not have the same state dict keys; do they have different architectures?')
+
+
+    def step(self):
+        one_minus_alpha = 1.0 - self.ema_alpha
+        for tgt_p, src_p in zip(self.target_params, self.source_params):
+            tgt_p.mul_(self.ema_alpha)
+            tgt_p.add_(src_p * one_minus_alpha)
+            
 
 @hydra.main(config_path='config', config_name="config.yaml")
 def main(cfg : DictConfig) -> None:
@@ -90,14 +115,18 @@ def main(cfg : DictConfig) -> None:
     
     val_loader = DataLoader(valid_dataset, **cfg.dataset.dataloader.valid)
     #valid_callback = ValidWholeImageCallback(dataset['valid_img_id'], cfg.dataset, cfg.trainer.max_epochs/2, 10)
+    
     # set model
-    model = get_model(cfg.model)
+    s_model = get_model(cfg.model)
+    t_model = get_model(cfg.model)
+    t_optim = EMAWeightOptimizer(t_model, s_model, teacher_alpha=0.99)
+    
     if cfg.model.ssl_model is not None:
         print('Loading ssl model: {}'.format(cfg.model.ssl_model))
         model.load_state_dict(custom_load(cfg.model.ssl_model), strict=False)
 
     # set lit system
-    lit_model = LitClassifier(hparams=cfg, model=model)
+    lit_model = LitClassifier(hparams=cfg, s_model=s_model, t_model=t_model, t_optim=t_optim)
 
     # set trainer
     trainer = Trainer(
