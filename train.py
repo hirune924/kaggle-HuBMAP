527324b78b45dbb91edba2cae183c380d9c66f18
hirune924 <36130517+hirune924@users.noreply.github.com>
2021-01-18 17:09:11 +0900

    Update train.py

--- train.py
+++ train.py
@@ -8,7 +8,7 @@ from pytorch_lightning import Trainer
 from pytorch_lightning.callbacks import LearningRateMonitor, EarlyStopping, ModelCheckpoint
 
 from model.model import get_model
-from dataset.hubmap import get_dataset2
+from dataset.hubmap_ssl import get_dataset2
 from system.system import LitClassifier
 from callback.callback import ValidWholeImageCallback
 from utils.utils import custom_load
@@ -17,6 +17,57 @@ from utils.utils import custom_load
 from torchvision.datasets import CIFAR10
 from torchvision import transforms
 from torch.utils.data import DataLoader, random_split
+
+from __future__ import print_function
+import torchvision
+from torch.utils.data import DataLoader
+from torchvision import transforms
+
+
+class MyIter(object):
+  """An iterator."""
+  def __init__(self, my_loader):
+    self.my_loader = my_loader
+    self.loader_iters = [iter(loader) for loader in self.my_loader.loaders]
+
+  def __iter__(self):
+    return self
+
+  def __next__(self):
+    # When the shortest loader (the one with minimum number of batches)
+    # terminates, this iterator will terminates.
+    # The `StopIteration` raised inside that shortest loader's `__next__`
+    # method will in turn gets out of this `__next__` method.
+    batches = [loader_iter.next() for loader_iter in self.loader_iters]
+    return self.my_loader.combine_batch(batches)
+
+  # Python 2 compatibility
+  next = __next__
+
+  def __len__(self):
+    return len(self.my_loader)
+
+  
+class MyLoader(object):
+  """This class wraps several pytorch DataLoader objects, allowing each time 
+  taking a batch from each of them and then combining these several batches 
+  into one. This class mimics the `for batch in loader:` interface of 
+  pytorch `DataLoader`.
+  Args: 
+    loaders: a list or tuple of pytorch DataLoader objects
+  """
+  def __init__(self, loaders):
+    self.loaders = loaders
+
+  def __iter__(self):
+    return MyIter(self)
+
+  def __len__(self):
+    return min([len(loader) for loader in self.loaders])
+
+  # Customize the behavior of combining batches here.
+  def combine_batch(self, batches):
+    return batches
 
 @hydra.main(config_path='config', config_name="config.yaml")
 def main(cfg : DictConfig) -> None:
@@ -36,9 +87,13 @@ def main(cfg : DictConfig) -> None:
     # set data
     dataset = get_dataset2(cfg.dataset)
     train_dataset = dataset['train']
+    train_unlabeled = dataset['train_unlabel'] 
     valid_dataset = dataset['valid']
 
-    train_loader = DataLoader(train_dataset, **cfg.dataset.dataloader.train)
+    train_labeled_loader = DataLoader(train_dataset, **cfg.dataset.dataloader.train)
+    train_unlabeled_loader = DataLoader(train_unlabeled, **cfg.dataset.dataloader.train)
+    train_loader = MyLoader([train_labeled_loader, train_unlabeled_loader])
+    
     val_loader = DataLoader(valid_dataset, **cfg.dataset.dataloader.valid)
     #valid_callback = ValidWholeImageCallback(dataset['valid_img_id'], cfg.dataset, cfg.trainer.max_epochs/2, 10)
     # set model
